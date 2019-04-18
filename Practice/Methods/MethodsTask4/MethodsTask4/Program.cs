b236cc96b33480829cf95359e5ff96b6dabc12a6
KyleBlackwell01 <48078043+KyleBlackwell01@users.noreply.github.com>
2019-04-18 17:06:10 +1000

    MethodsTask4

---
+++ Practice/Methods/MethodsTask4/MethodsTask4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodsTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            swapInteger(ref num1, ref num2);
+
+
+
+            Console.WriteLine(num1 + " " + num2);
+
+
+            Console.ReadKey();
+        }
+
+        static void swapInteger(ref int num1, ref int num2)
+        {
+
+           if (num1 != num2)
+            {
+                num1 ^= num2;
+                num2 ^= num1;
+                num1 ^= num2;
+
+            }
+           
+        }
+    }
+}
