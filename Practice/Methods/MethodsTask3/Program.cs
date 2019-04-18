a5519f4cfd785f4e9485a3d8fd46924cc640447a
KyleBlackwell01 <48078043+KyleBlackwell01@users.noreply.github.com>
2019-04-18 16:40:43 +1000

    Methods Practice
    Task 3

---
+++ Practice/Methods/MethodsTask3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+
+
+            Console.WriteLine("Enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result;
+
+
+            result = p.AddTwoNumbers(num1, num2);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+            
+            
+        }
+
+        public int AddTwoNumbers(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        
+    }
+}
