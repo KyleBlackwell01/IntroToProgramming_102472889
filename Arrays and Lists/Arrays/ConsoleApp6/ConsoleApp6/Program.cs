ffa9144295662b1c6c066f0d7f6646e63946eb43
KyleBlackwell01 <48078043+KyleBlackwell01@users.noreply.github.com>
2019-04-18 15:48:30 +1000

    Array

---
+++ Arrays and Lists/Arrays/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int count = 0;
+
+            int[] scorearray = new int[20];
+
+
+            
+            while(count < 20)
+            {
+                Console.Write("Input Score:  ");
+                int score = int.Parse(Console.ReadLine());
+                scorearray[count] = score;
+                total += score;
+                count++;
+            }
+
+            int avg = total / 20;
+
+            Console.WriteLine("Total Average: " + avg);
+
+
+            Console.ReadKey();
+        }
+    }
+}
